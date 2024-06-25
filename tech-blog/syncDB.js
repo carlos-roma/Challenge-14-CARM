156af6e04f3efc91bc431f965fca18505143ba27
carlos-roma <adrianc.rm0@gmail.com>
2024-06-25 05:14:00 -0600

    uploading v2, db matching the models and functionalities debugged, dashboard delete and edit

--- tech-blog/syncDB.js
+++
@@ -1,9 +0,0 @@
-const sequelize = require('./config/connection');
-
-const syncDatabase = async () => {
-  await sequelize.sync({ force: true }); // Use force: true to drop and recreate tables
-  console.log('Database synchronized');
-  process.exit(0);
-};
-
-syncDatabase();
